85a4343a8edd9902941400b13dd44fe371a469f3
nakagawa1210 <pxpf6xhb@s.okayama-u.ac.jp>
2021-06-21 13:39:07 +0900

    Add template for sourse_url_controller.rb

---
+++ lib/sourse_url_controller.rb
@@ -0,0 +1,17 @@
+class SoursceURLController
+    def initialize ()
+        #read register.json
+    end
+
+    def add (url,register_name)
+        #add url to registere.json
+    end
+
+    def delete (url,register_name)
+        #delete url of registere.json
+    end
+
+    def list ()
+        #read register.json
+    end
+end
