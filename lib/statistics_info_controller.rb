f84788687c83b4e10697624b3c920ed34ffdca31
nakagawa1210 <pxpf6xhb@s.okayama-u.ac.jp>
2021-07-05 14:08:35 +0900

    Add create table

--- lib/statistics_info_controller.rb
+++ lib/statistics_info_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class StatisticsInfoController
     def initialize()
 
@@ -13,23 +15,59 @@ class StatisticsInfoController
 
     def create_single_year_table(product_number,product_name,group_name,
         submission_number,submission_average,submission_sum,year)
+        label = ["成果物通番","成果物名"] 
+        label.concat group_name
 
+        product_number.length.times do |i|
+            table[i] = [product_number[i],product_name[i]]
+            group_name.length.times do |j|
+                table[i].push(submission_number[j][i])
+            end
+        end
+
+        botomlabel = ["提出回数平均",""]
+        botomlabel.concat submission_average
+
+        table.unshift label
+        table.push botomlabel
+
+        return table
     end
 
-    def create_multiple_years_table()
+    def create_multiple_years_table(group_name,submission_average,years)
+        label = [""]
+        label.concat group_name
 
+        statistics_year.each_with_index do |i,data|
+            table[i].push(data.@year)
+            table[i].concat data.@submission_average
+        end
+     
+        table.unshift label
+
+        return table
     end
 
     def create_graph(group_name, submission_average, year)
 
     end
 
-    def create_single_year_csv_file (create_single_year_table)
-
+    def create_single_year_csv_file (s_y_table)
+        s_y_csv = CSV.generate do |csv|
+            s_y_table.each do |data|
+                csv << data
+            end
+        end
+        return s_y_csv
     end
 
-    def create_multiple_years_csv_file(create_multiple_years_table)
-
+    def create_multiple_years_csv_file(m_y_table)
+        m_y_csv = CSV.generate do |csv|
+            m_y_table.each do |data|
+                csv << data
+            end
+        end
+        return m_y_csv
     end
 
     def push (grop="0年度0班", remarks = "0-000-nodata-0")
